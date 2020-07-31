b4cf18850bb713ec18fe7bae845ff62816690c82
PaPa31 <visidoor@yandex.ru>
2020-07-31 10:38:09 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -18,7 +18,7 @@ exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
 
   // Only update the `/app` page.
-  if (page.path.match(/^\/tutorial-part-four/)) {
+  if (page.path.match(/^\/)) {
     // page.matchPath is a special key that's used for matching pages
     // with corresponding routes only on the client.
     page.matchPath = "/tutorial-part-four/*"
